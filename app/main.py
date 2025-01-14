e80626fe38a1e33cec73ea87ea54c50923fc47ef
sdkim96 <seong960603@gmail.com>
2025-01-14 22:47:19 +0900

    af

--- app/main.py
+++ app/main.py
@@ -157,14 +157,24 @@ async def get_student_result_only_meta(
 @app.get("/api/students", response_model=GetStudentsResponse)
 async def get_students(me: User = Depends(get_current_user), db: Session = Depends(get_db)):
         
-        if me.user_type not in [UType.TEACHER, UType.ADMIN]:
-            raise HTTPException(status_code=403, detail="You are not a teacher")
-        
-        all_user = User.get_all(db)
-        if not all_user:
-            raise HTTPException(status_code=404, detail="No student found")
-        
-        return GetStudentsResponse(students=[UserDTO(id=each.id, name=each.name) for each in all_user if each.user_type == UType.STUDENT])
+    if me.user_type not in [UType.TEACHER, UType.ADMIN]:
+        raise HTTPException(status_code=403, detail="You are not a teacher")
+    
+    all_user = User.get_all(db)
+    if not all_user:
+        raise HTTPException(status_code=404, detail="No student found")
+    
+    return GetStudentsResponse(
+        students=[
+            UserDTO(
+                id=each.id, 
+                name=each.name,
+                user_name=each.user_name,
+                user_nickname=each.user_nickname,
+            ) 
+            for each in all_user if each.user_type == UType.STUDENT
+        ]
+    )
 
 
 @app.post("/api/sign_up", response_model=UserDTO)
